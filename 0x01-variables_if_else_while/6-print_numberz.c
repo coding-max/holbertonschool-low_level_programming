45d6877d076689d395e767722f332f9563662588
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-02-04 17:12:13 +0000

    add 5-6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 48; digit != 58; digit++)
+		putchar(digit);
+	putchar('\n');
+	return (0);
+}
