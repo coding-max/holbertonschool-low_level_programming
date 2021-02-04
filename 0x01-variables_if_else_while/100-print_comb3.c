24d72333de8e4bbb3bbbc1a57df29f5f41c7d77b
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-02-04 20:02:21 +0000

    add 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x, y;
+
+	x = 48;
+	while (x < 57)
+	{
+		y = x + 1;
+		while (y < 58)
+		{
+			putchar(x);
+			putchar(y);
+			if (x == 56 && y == 57)
+			{
+				putchar('\n');
+			} else
+			{
+				putchar(44);
+				putchar(32);
+			}
+			y++;
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
