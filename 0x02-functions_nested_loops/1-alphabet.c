cb9e2cd9d239a4486eebb55990f709c3e0b09ef5
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-02-09 12:40:34 +0000

    add 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+		_putchar(c);
+	_putchar('\n');
+
+	return (0);
+}
