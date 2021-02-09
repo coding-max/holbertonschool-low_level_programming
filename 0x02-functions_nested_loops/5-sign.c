fe14a8af2a731e4b3d9deb70cbe75b0ff1528305
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-02-09 13:49:55 +0000

    edit 5 (add _putchar)

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,9 +10,18 @@
 int print_sign(int n)
 {
 	if (n < 0)
+	{
+		_putchar('-');
 		return (-1);
+	}
 	else if (n > 0)
+	{
+		_putchar('+');
 		return (1);
+	}
 	else
+	{
+		_putchar('0');
 		return (0);
+	}
 }
