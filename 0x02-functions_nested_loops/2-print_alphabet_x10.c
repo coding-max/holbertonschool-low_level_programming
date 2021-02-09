e2ef1dbb6fdd6e83c5b994b9ea2e52e8620d6842
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-02-09 12:51:26 +0000

    add 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet.
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+		for (c = 'a' ; c <= 'z' ; c++)
+			_putchar(c);
+	_putchar('\n');
+}
