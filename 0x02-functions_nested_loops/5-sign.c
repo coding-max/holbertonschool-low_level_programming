570650720a98ae495e518827b118aeca224b2a9d
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-02-09 13:45:52 +0000

    add 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdbool.h>
+
+/**
+ * print_sign - checks the sign of an int number
+ * @n: int to check
+ *
+ * Return: 1 (n>0), 0 (n=0) and -1 (n<0)
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n > 0)
+		return (1);
+	else
+		return (0);
+}
