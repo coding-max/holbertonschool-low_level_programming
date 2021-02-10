e449ac59773fa48d283a529ab366082938324d03
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-02-10 00:59:52 +0000

    edit 1 (fix betty)

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
-        i = 0;
+	i = 0;
 
  /*
   * while (i < 10)
@@ -20,7 +20,7 @@ int main(void)
   * }
   */
 
-        printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
-        return (0);
+	return (0);
 }
