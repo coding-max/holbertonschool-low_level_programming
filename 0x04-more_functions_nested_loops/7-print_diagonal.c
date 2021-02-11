7f2c4e8df8b75e55e5a2b2eeb79d7b0cf561268e
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-02-11 15:56:06 +0000

    edit 7 (add \n)

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,14 +7,16 @@
  */
 void print_diagonal(int n)
 {
-	int i, spaces;
+	int i, j, spaces;
 
 	spaces = 0;
 	for (i = n; i > 0; i--)
 	{
-		for (j = spaces; i > 0; i--)
+		for (j = spaces; j > 0; j--)
 			_putchar(' ');
 		_putchar(92);
+		_putchar('\n');
 		spaces++;
 	}
+	_putchar('\n');
 }
