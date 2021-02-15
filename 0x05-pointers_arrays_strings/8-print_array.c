4c85e975216f64992870205d68a3038f3884472f
Maximilinao <maximiliano.pan@hotmail.com>
2021-02-15 19:51:00 -0300

    edit 8 (change _putchar for printf)

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * print_array - prints n elements of an array of integers, followed by a \n.
@@ -10,11 +11,7 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
-	{
-		_putchar(i);
-		_putchar(',');
-		_putchar(' ');
-	}
-	_putchar('\n');
+	for (i = 0; i < (n - 1); i++)
+		printf("%i, ", a[i]);
+	printf("%i\n", a[i]);
 }
