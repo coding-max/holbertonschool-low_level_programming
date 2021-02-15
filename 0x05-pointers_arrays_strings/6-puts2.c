119574db20fa1f6c5b88c0d6a7bf6724fc3b1c84
Maximilinao <maximiliano.pan@hotmail.com>
2021-02-15 18:08:26 -0300

    add files 6-7

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints every other character of a string.
+ * starting with the first character, followed by a new line.
+ * @str: string.
+ *
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints half of a string, followed by a new line.
+ * @str: string.
+ *
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	i = i / 2;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
