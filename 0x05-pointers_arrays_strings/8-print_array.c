4c73068efd091bcdeb6b4f57176bfd2d3c73749e
Maximilinao <maximiliano.pan@hotmail.com>
2021-02-16 17:22:02 -0300

    edit 8 (add condition for border case)

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,7 +11,8 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i + 1 < n; i++)
+	for (i = 0; i < (n - 1); i++)
 		printf("%d, ", a[i]);
-	printf("%d\n", a[i]);
+	if (i != 0)
+		printf("%d\n", a[i]);
 }
