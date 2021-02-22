edecdd154f7d9eef6edf5f7ce1d14cd240281167
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-02-22 18:39:37 +0000

    update 6 (betty errors fixed)

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -24,8 +24,8 @@ char *cap_string(char *s)
 				break;
 			}
 		}
-		if (cond && (s[i+1] >= 'a' && s[i+1]<= 'z'))
-			s[i+1] = (s[i+1] - 32);
+		if (cond && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+			s[i + 1] = (s[i + 1] - 32);
 		cond = 0;
 	}
 	return (s);
