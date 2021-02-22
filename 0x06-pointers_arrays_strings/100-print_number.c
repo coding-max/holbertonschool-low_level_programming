17100878b2f64218181685b9aee7261bb5ed6598
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-02-22 16:06:47 +0000

    add 100-print_number.c and 101-magic.c

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_number -  prints an integer.
+ * @n: number to print.
+ *
+ */
+
+void print_number(int n)
+{
+	int num;
+
+	if (n >= 0)
+		num = n;
+	else
+	{
+		num = -n;
+		_putchar('-');
+        }
+	/* recursion */
+	if (num / 10 != 0)
+		print_number(num / 10);
+	_putchar((num % 10) + 48);
+}
