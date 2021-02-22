5e700d76abc179d2df7dc6e1bd0dcc8855c67908
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-02-22 18:36:06 +0000

    update 6 (remake)

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,13 +8,25 @@
  */
 char *cap_string(char *s)
 {
-	int i = 0;
+	int i = 0, j, cond;
+	char sep[] = " \t\n,;.!?\"(){}";
 
-	for (; s[i] != '\0'; i++)
+	if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
+		s[i] = (s[i] - 32);
+
+	for (cond = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == ' ' || s[i] == '.' || s[i] == '\n' || s[i] == '\t')
-			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-				s[i + 1] = s[i + 1] - 32;
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (s[i] == sep[j])
+			{
+				cond = 1;
+				break;
+			}
+		}
+		if (cond && (s[i+1] >= 'a' && s[i+1]<= 'z'))
+			s[i+1] = (s[i+1] - 32);
+		cond = 0;
 	}
 	return (s);
 }
