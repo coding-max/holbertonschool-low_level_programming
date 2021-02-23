15cd9cd1b24631f31bd5fd421198287a0b666e40
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-02-23 14:32:07 +0000

    added: 5 (preliminary)

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * _strstr - locates a substring.
+ * @haystack: string.
+ * @needle: substring.
+ *
+ * Return: pointer to the beginning of the located substring.
+ * NULL in other case.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j = 0, exit = 0;
+	char *ptr;
+
+	for (i = 0; (haystack[i] != '\0'); i++)
+		if (haystack[i] == needle[j])
+		{
+			ptr = &haystack[i];
+			break;
+		}
+	if (haystack[i] == '\0')
+		return ('\0');
+	else
+	{
+		while ((haystack[i] != '\0') && (needle[j] != '\0'))
+		{
+			if (haystack[i] != needle[j])
+			{
+				exit = 1;
+				break;
+			}
+			i++;
+			j++;
+		}
+		if (exit || needle != '\0')
+			return ('\0');
+		else
+			return (ptr);
+	}
+}
