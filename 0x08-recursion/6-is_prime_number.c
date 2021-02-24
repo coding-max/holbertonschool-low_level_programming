3b02ae66b402938eaa0804b2c90e933a0b0549b6
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-02-24 21:59:10 +0000

    added: 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * prime_function - auxiliar function.
+ * @n: int to evaluate.
+ * @i: auxiliar var.
+ *
+ * Return: 1 or 0.
+ */
+int prime_function(int n, int i)
+{
+	if (i == 2)
+		return (1);
+	if ((n % i) == 0)
+		return (0);
+	return (prime_function(n, i - 1));
+}
+
+/**
+ * is_prime_number - return 1 if the input integer is a prime number,
+ * otherwise return 0.
+ * @n: number to evaluate.
+ *
+ * Return: 1 or 0.
+ */
+int is_prime_number(int n)
+{
+	if (n > 2)
+	{
+		int i = (n / 2);
+		return (prime_function(n, i));
+	}
+	if (n < 2)
+		return (0);
+	else
+		return (1);
+}
