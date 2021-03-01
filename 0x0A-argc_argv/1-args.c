58f0a2477152f9b1b23bbb4aefe52de856126131
Maximilinao <maximiliano.pan@hotmail.com>
2021-02-28 22:03:55 -0300

    added: files 1-4

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into the program.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", *argc - 1);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - prints all arguments recived.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
