2143dd45953fb706809fe2bf1c95ffd4fc2581b6
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-03-01 00:25:10 +0000

    added: task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the program followed by a new line.
+ * @argc:
+ * @argv:
+ *
+ * Return: 0.
+ */
+
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", *argv);
+
+	return (0);
+}
