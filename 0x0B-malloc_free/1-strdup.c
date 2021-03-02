e02dd066b6193b58177f4264cca77969d5eaa19f
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-03-02 17:21:04 +0000

    added: task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string.
+ *
+ * Return: Length of @s.
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	int cont = 0;
+
+	while (s[i] != '\0')
+	{
+		cont++;
+		i++;
+	}
+	return (cont);
+}
+
+/**
+ * _strcpy - copy the string pointed to by src inluding the null byte
+ * to the buffer pointed to by dest.
+ * @dest: destination.
+ * @src: source.
+ *
+ * Return: @direction = pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *direction = dest;
+
+	while (*src)
+		*dest++ = *src++;
+	*dest = 0;
+	return (direction);
+}
+
+/**
+ * _strdup - returns a pointer to a new string which is a duplicate of str.
+ * @str: source string to duplicate.
+ *
+ * Return: pointer to the duplicated string.
+ * or NULL if str = NULL or if insufficient memory was available.
+ */
+char *_strdup(char *str)
+{
+	char *aux = malloc(_strlen(str) + 1);
+
+	if (aux == NULL || str == NULL)
+		return (NULL);
+	return (_strcpy(aux, str));
+}
