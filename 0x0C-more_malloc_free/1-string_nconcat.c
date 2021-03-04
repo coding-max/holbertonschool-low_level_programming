af845ad5f45874b10c4661251d709852754c063b
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-03-04 18:38:12 +0000

    updated: task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 unsigned int _strlen(char *s);
-char *_strcat(char *dest, char *src);
 
 /**
  * string_nconcat - concatenates two strings,
@@ -56,26 +55,4 @@ unsigned int _strlen(char *s)
 		i++;
 	}
 	return (cont);
-}
-
-/**
- * *_strcat - concatenates two strings.
- * @dest: destination.
- * @src: source.
- * Return: pointer to the resulting string dest.
- */
-char *_strcat(char *dest, char *src)
-{
-	int i, j = 0;
-	char *ptr = dest;
-
-	if (src == NULL)
-		return (ptr);
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-	}
-	for (; src[j] != '\0'; i++, j++)
-		dest[i] = src[j];
-	dest[i + 1] = src[j + 1];
-	return (ptr);
 }
