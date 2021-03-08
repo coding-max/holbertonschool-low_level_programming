c070fa10146eb6da2bd1ff472307a2b542707a96
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-03-08 18:40:09 +0000

    added: dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog.
+ * @name: dog's name.
+ * @age: dog's age.
+ * @owner: dog's owner.
+ **/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
