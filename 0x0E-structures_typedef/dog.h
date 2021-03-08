4c78ffc9112092fa7c30fa4a27f1995e6ae6aab2
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-03-08 19:11:03 +0000

    updated: task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,10 +14,11 @@ typedef struct dog
 	char *owner;
 } dog;
 
+typedef struct dog dog_t;
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-int _putchar(char c);
 
 #endif
