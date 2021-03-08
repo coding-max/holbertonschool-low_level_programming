02a944b2c600d0c4f875b6128bf091debdc015da
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-03-08 18:56:33 +0000

    added: task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable of type struct dog.
+ * @d: dog structure.
+ * @name: dog's name.
+ * @age: dog's age.
+ * @owner: dog's owner.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
