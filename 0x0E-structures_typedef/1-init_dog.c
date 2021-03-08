0031169597b6811ae4f8bba594c1d6cd859d1032
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-03-08 19:04:07 +0000

    updated: task 1 (rm ;)

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,7 +8,7 @@
  * @age: dog's age.
  * @owner: dog's owner.
  */
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
