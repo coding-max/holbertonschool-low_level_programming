1c98f178b67f6e5920624f57828cfd7bda7685cb
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-03-11 18:06:25 +0000

    added: readme & library

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef VARDIAC_FUNCTIONS
+#define VARDIAC_FUNCTIONS
+#include <stdarg.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
