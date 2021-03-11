b337545a53f3436c2427715e6afcdf5459341283
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-03-11 00:54:11 +0000

    updated: library (+ <stdio.h>)

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define FUNCTION_POINTERS
 #include <stdlib.h>
 #include <stddef.h>
+#include <stdio.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
