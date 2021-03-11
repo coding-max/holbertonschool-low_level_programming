6a5641ef4890a3600ff0e992c0de6defc178c735
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-03-11 20:44:58 +0000

    updated: library (struct for task 3)

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,4 +10,15 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct format - struct for task 3.
+ * @format_in: id of format
+ * @function: pointer to function that print with @format_in
+ **/
+typedef struct format
+{
+	char format_in;
+	void (*function)(va_list);
+} format_t;
+
 #endif
