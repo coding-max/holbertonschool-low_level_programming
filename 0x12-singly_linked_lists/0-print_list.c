0b1d9de1714478eaa47c4bd5e052494b68d92eef
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-03-18 11:20:08 -0300

    added: task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: number of parameters.
+ * Return: number of nodes.
+ **/
+size_t print_list(const list_t *h)
+{
+        list_t *actual_node = h;
+        size_t cont = 0;
+
+        if (!actual_node)
+        {
+                printf("[0] (nil)");
+        }
+
+        while (actual_node->next)
+        {
+                if (actual_node->str)
+                        printf("[0] (nil)");
+                else
+                        printf("[%d] %s", actual_node->len, actual_node->str);
+                cont++;
+                actual_node = actual_node->next;
+        }
+        if (actual_node->str)
+                printf("[0] (nil)");
+        else
+                printf("[%d] %s", actual_node->len, actual_node->str);
+        cont++;
+
+        return (cont);
+}
