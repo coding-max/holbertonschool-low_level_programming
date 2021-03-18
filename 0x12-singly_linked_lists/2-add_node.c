79d8dda66739c94f2171269185eeb5f2e2370bbf
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-03-18 14:32:21 -0300

    added: task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+int _strlen_recursion(char *s);
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list.
+ * @head: pointer to the list.
+ * @str: pointer to the list.
+ * Return: address of the new element, or NULL if it failed
+ **/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	new->str = strdup(str);
+	new->len = _strlen_recursion(new->str);
+	new->next = head;
+	*head = new;
+
+	return (new);
+}
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string.
+ * Return: length of @s.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
