90bc37020baa18d03a3f5b498e1bd031c3b7353c
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-03-18 15:38:53 -0300

    added: task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list.
+ * @h: pointer to the list.
+ **/
+void free_list(list_t *head)
+{
+	list_t *actual_node;
+	list_t *next_node;
+
+	if (head)
+	{
+		actual_node = head;
+		next_node = head->next;
+		while (next_node)
+		{
+			free(actual_node);
+			actual_node = next_node;
+			next_node = next_node->next;
+		}
+		free(actual_node);
+	}
+}
