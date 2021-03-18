f4bdd54830a5604c1af0bac7628eabe3b240f489
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-03-18 15:05:37 -0300

    fixing

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -26,7 +26,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	new->len = _strlen_recursion(new->str);
         new->next = NULL;
 
-        if (head)
+        if (aux)
         {
                 while (aux->next)
                         aux = aux->next;
