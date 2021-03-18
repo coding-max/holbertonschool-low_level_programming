b7cdbad547f99e6c5fdca9fb9d43c4a64c576d50
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-03-18 13:19:20 -0300

    updated: task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,22 +10,23 @@ size_t print_list(const list_t *h)
         size_t cont = 0;
 
         if (!h)
-                printf("[0] (nil)");
-
-        while (h->next)
-        {
-                if (h->str)
-                        printf("[0] (nil)");
-                else
-                        printf("[%d] %s", h->len, h->str);
-                cont++;
-                h = h->next;
-        }
-        if (h->str)
-                printf("[0] (nil)");
+                printf("[0] (nil)\n");
         else
-                printf("[%d] %s", h->len, h->str);
-        cont++;
-
+        {
+                while (h->next)
+                {
+                        if (h->str)
+                                printf("[0] (nil)\n");
+                        else
+                                printf("[%u] %s\n", h->len, h->str);
+                        cont++;
+                        h = h->next;
+                }
+                if (h->str)
+                        printf("[0] (nil)\n");
+                else
+                        printf("[%u] %s\n", h->len, h->str);
+                cont++;
+        }
         return (cont);
 }
