ca2d5201e4e0155ca82e52ca6c462ac9d1042ee8
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-03-18 14:56:15 -0300

    added: task 2

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+
+int _strlen_recursion(char *s);
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list.
+ * @head: pointer to the first element of the list.
+ * @str: string to set in the new node.
+ * Return: address of the new element, or NULL if it failed
+ **/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *aux = *head;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	if (!new->str)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = _strlen_recursion(new->str);
+        new->next = NULL;
+
+        if (head)
+        {
+                while (aux->next)
+                        aux = aux->next;
+                aux->next = *new;
+        }
+        else 
+        {
+                head = *new;
+        }
+
+	return (head);
+}
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string.
+ * Return: length of @s.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
