33bc01c72d71e579d116171d0c424d92aa2f84c0
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-03-22 13:49:49 -0300

    updated task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,17 +15,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int cont = 0;
 
 	/* create node */
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 		return (NULL);
-	new->n = n;
+	new_node->n = n;
 
 	/* border case for insert at the beginning */
 	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
-		return (**head);
+		return (*head);
 	}
 
 	/* search of position to insert */
@@ -41,7 +41,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new_node->next = aux_node->next;
 		aux_node->next = new_node;
-		return (*new_node);
+		return (new_node);
 	}
 
 	free(new_node);
