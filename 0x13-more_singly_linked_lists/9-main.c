98b8faf86c0c191a5025824426c43c9442222fd3
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-03-22 14:01:02 -0300

    upadted main file of task 9

--- 0x13-more_singly_linked_lists/9-main.c
+++ 0x13-more_singly_linked_lists/9-main.c
@@ -13,18 +13,27 @@ int main(void)
 	listint_t *head;
 
 	head = NULL;
+	print_listint(head);
+	printf("-----------------\n");
+	insert_nodeint_at_index(&head, 0, 34047);
+	print_listint(head);
+	free_listint2(&head);
+	printf("\n\n\n");
+
+	head = NULL;
 	add_nodeint_end(&head, 0);
 	add_nodeint_end(&head, 1);
 	add_nodeint_end(&head, 2);
 	add_nodeint_end(&head, 3);
-	add_nodeint_end(&head, 4);
-	add_nodeint_end(&head, 98);
-	add_nodeint_end(&head, 402);
-	add_nodeint_end(&head, 1024);
 	print_listint(head);
 	printf("-----------------\n");
-	insert_nodeint_at_index(&head, 5, 4096);
+	insert_nodeint_at_index(&head, 3, 34047);
+	print_listint(head);
+	printf("-----------------\n");
+	insert_nodeint_at_index(&head, 4, 34047);
 	print_listint(head);
 	free_listint2(&head);
+	printf("\n\n\n");
+
 	return (0);
 }
