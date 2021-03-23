4f2601f742579e6cd7899d65d9383a6fd9a7eab7
Maximilinao <maximiliano.pan@hotmail.com>
2021-03-23 15:28:48 -0300

    fixing ask 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,12 +7,15 @@
  **/
 int pop_listint(listint_t **head)
 {
-	int data = (*head)->n;
+	int data;
 	listint_t *first_node = *head;
 
+	if (!first_node)
+		return (0);
+
+	data = (*head)->n;
 	*head = first_node->next;
 	free(first_node);
-
 	return (data);
 }
 
