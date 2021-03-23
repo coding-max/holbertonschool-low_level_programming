256b5c7bb14de45e0744e9b9ada759c85078112a
Maximilinao <maximiliano.pan@hotmail.com>
2021-03-23 15:24:04 -0300

    fixing task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,17 +10,17 @@ void free_listint2(listint_t **head)
 	listint_t *actual_node;
 	listint_t *next_node;
 
-	if (head)
+	if (!head)
+		return;
+
+	actual_node = *head;
+	next_node = (*head)->next;
+	while (next_node)
 	{
-		actual_node = *head;
-		next_node = (*head)->next;
-		while (next_node)
-		{
-			free(actual_node);
-			actual_node = next_node;
-			next_node = next_node->next;
-		}
 		free(actual_node);
+		actual_node = next_node;
+		next_node = next_node->next;
 	}
+	free(actual_node);
 	*head = NULL;
 }
