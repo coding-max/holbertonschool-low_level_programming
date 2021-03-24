dcb742b89d07b381032424d58f16164c301ba283
Maximilinao <maximiliano.pan@hotmail.com>
2021-03-24 10:44:17 -0300

    upadted task 11

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,83 +1,25 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node at a given position.
+ * reverse_listint - reverses a listint_t linked list.
  * @head: pointer to the list.
- * @idx: position to add the node.
- * @n: data for the new node.
- * Return: the address of the new node, or NULL if it failed
+ * Return: pointer to the first node of the reversed list
  **/
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
-{
-	size_t length = listint_len(*head);
-	size_t i;
-	int data;
-	listint_t *h;
+ listint_t *reverse_listint(listint_t **head)
+ {
+	listint_t *prev_node, *next_node;
 
-	h = NULL;
-	for (i = 0; i < length; i++)
-	{
-		data = pop_node(get_node(*head, i));
-		h = add_node(&h, data);
-	}
-
-	return (h);
-}
-
-/**
- * pop_node - deletes the head node of a listint_t linked list.
- * @head: pointer to the list.
- * Return: head node’s data (n).
- **/
-int pop_node(listint_t **head)
-{
-	int data = (*head)->n;
-	listint_t *first_node = *head;
-
-	*head = first_node->next;
-	free(first_node);
-
-	return (data);
-}
-
-/**
- * get_node - returns the nth node of a listint_t linked list.
- * @head: pointer to the list.
- * @index: nth node.
- * Return: nth node, if the node doesn't exist, returns NULL.
- **/
-listint_t *get_node(listint_t *head, unsigned int index)
-{
-	listint_t *node = head;
-	unsigned int cont = 0;
-
-	while (node && cont != index)
-	{
-		cont++;
-		node = node->next;
-	}
-	if (node && cont == index)
-		return (node);
-	return (NULL);
-}
-
-/**
- * add_node - adds a new node at the beginning of a listint_t list.
- * @head: pointer to the first element of the list.
- * @n: int to set in the new node.
- * Return: address of the new element, or NULL if it failed
- **/
-listint_t *add_node(listint_t *head, const int n)
-{
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	if (!head)
 		return (NULL);
 
-	new->n = n;
-	new->next = head;
-	head = new;
-
-	return (new);
-}
+	prev_node = NULL;
+	while (*head)
+	{
+		next_node = (*head)->next;
+		(*head)->next = prev_node;
+		prev_node = *head;
+		*head = next_node;
+	}
+	*head = prev_node;
+	return (*head);
+ }
