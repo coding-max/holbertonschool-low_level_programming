548a3a5de5b50775fc654a044e32c97a0fd6af84
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-03-30 00:09:29 -0300

    fixing gcc

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,10 @@
 #include "holberton.h"
 #include <stdio.h>
 
+int close_error(int fd);
+void read_error(char *filename);
+void write_error(char *filename);
+
 /**
  * main - copies the content of a file to another file.
  * @argc: argument counter.
