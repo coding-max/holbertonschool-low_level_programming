40a4cac53044231e2ad47fead09f8758ee2fe9f8
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-09-01 17:37:45 +0000

    added header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH
+#define SEARCH
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
